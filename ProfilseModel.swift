61248e6bdd96d19ed2b967d64776868a635524be
emirisama <emirin.ayu.vn@gmail.com>
2021-03-30 00:10:28 +0900

    Profile情報をFIrebaseへ送信（UserDefaultsEX）

---
+++ ProfilseModel.swift
@@ -0,0 +1,19 @@
+//
+//  ProfilseModel.swift
+//  GR
+//
+//  Created by 中森えみり on 2021/03/29.
+//
+
+import Foundation
+
+
+struct ProfileModel:Codable {
+        
+    var name:String?
+    var id:String?
+    var email:String?
+    
+    
+
+}
