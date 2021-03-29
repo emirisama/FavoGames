3ccd4a76290e043d028c8a91068223321665bc35
emirisama <emirin.ayu.vn@gmail.com>
2021-03-30 00:10:28 +0900

    Profile情報をFIrebaseへ送信（UserDefaultsEX）

---
+++ GR/Controller/MypageViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MypageViewController.swift
+//  GR
+//
+//  Created by 中森えみり on 2021/03/28.
+//
+
+import UIKit
+import Foundation
+
+
+class MypageViewController: UIViewController {
+
+
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var introductionLabel: UILabel!
+    @IBOutlet weak var profielImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        }
+        
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
