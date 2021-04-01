eec25d7b9fe9687af15698ed936ac19c2ab8c248
emirisama <emirin.ayu.vn@gmail.com>
2021-04-01 22:04:25 +0900

    Mypageのカメラ立ち上げ機能作成

---
+++ GR/Controller/CameraViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CameraViewController.swift
+//  GR
+//
+//  Created by 中森えみり on 2021/03/31.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    var index = Int()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
