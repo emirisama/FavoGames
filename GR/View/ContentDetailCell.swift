164d6b5c3b6ec9a94cde6c0ae7d0da9fd64535fc
emirisama <emirin.ayu.vn@gmail.com>
2021-05-25 23:44:35 +0900

    楽天APIを用いてゲームタイトル検索画面からゲームタイトルを検索して一覧を表示させる、レビュー投稿機能実装にあたりReViewVCやDetailVCを修正し、getGameTitleメソッドを作成

---
+++ GR/View/ContentDetailCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContentDetailCell.swift
+//  GR
+//
+//  Created by 中森えみり on 2021/05/24.
+//
+
+import UIKit
+
+class ContentDetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ LikeModel.swift
@@ -0,0 +1,8 @@
+//
+//  LikeModel.swift
+//  GR
+//
+//  Created by 中森えみり on 2021/05/18.
+//
+
+import Foundation
